4f0dd58a019489899301e9f0328ab34d0a6d518f
Kiyohito Itoh <kiyohito.itoh@gmail.com>
2020-08-26 20:48:37 +0900

    Redisのヘルスチェックを追加。

---
+++ src/main/java/nablarch/integration/health/RedisHealthChecker.java
@@ -0,0 +1,43 @@
+package nablarch.integration.health;
+
+import nablarch.fw.ExecutionContext;
+import nablarch.fw.web.HttpRequest;
+import nablarch.fw.web.handler.health.HealthChecker;
+import nablarch.integration.redisstore.lettuce.LettuceRedisClient;
+
+/**
+ * Redisのヘルスチェックを行うクラス。
+ *
+ * キーの存在チェックを行い、例外が発生しなければヘルシと判断する。
+ * キーのデフォルトは"healthcheck"。
+ * キーは存在しなくてよい。
+ *
+ * @author Kiyohito Itoh
+ */
+public class RedisHealthChecker extends HealthChecker {
+
+    private LettuceRedisClient client;
+    private String key = "healthcheck";
+
+    @Override
+    protected boolean tryOut(HttpRequest request, ExecutionContext context) {
+        client.exists(key);
+        return true;
+    }
+
+    /**
+     * Redisのクライアントを設定する。
+     * @param client Redisのクライアント
+     */
+    public void setClient(LettuceRedisClient client) {
+        this.client = client;
+    }
+
+    /**
+     * 存在チェックに使用するキーを設定する。
+     * @param key 存在チェックに使用するキー
+     */
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
